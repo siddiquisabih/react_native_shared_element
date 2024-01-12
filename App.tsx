7b78ac4d5fe2bc1977e86c46a336decb6f9998b3
sabih.siddiqui <sabih.siddiqui@crowngroup.com.pk>
2024-01-12 17:36:21 +0500

    shared element small expample

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainListing from './src/mainlisting';
+import ListDetail from './src/listDetail';
+import Route from './src/route';
+
+
+function App(): React.JSX.Element {
+
+  // return <MainListing />
+  // return <ListDetail />
+  return <Route />
+}
+
+export default App;
